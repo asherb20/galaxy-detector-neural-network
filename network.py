36bdd8adb504696a1adb2637f1dfd51cd28c7c4e
Asher Best <ashermcbest@gmail.com>
2025-12-24 09:13:55 -0600

    revised forward pass function to calculate all activations and weighted inputs in a given layer and wrote 1st version of backprop function

--- network.py
+++ network.py
@@ -11,16 +11,50 @@ class Network:
 
    def sigmoid(self, z):
       return 1.0 / (1.0 + np.exp(-z))
-
-   def forward_pass(self, x):
-      a = x.reshape(-1, 1)
-      for w, b in zip(self.weights, self.biases):
-         z = np.dot(w, a) + b
-         a = self.sigmoid(z)
-      return a
+   
+   def sigmoid_prime(self, z):
+      return self.sigmoid(z) * (1 - self.sigmoid(z))
    
    def binary_cross_entropy(self, y, a):
       return -(y * np.log(a + 1e-8) + (1 - y) * np.log(1 - a + 1e-8))
+
+   # iterate over each layer
+   # calculate the activation for each neuron
+   # return all activations and the weighted inputs (zs)
+   def forward_pass(self, x):
+      activation = x.reshape(-1, 1)
+      activations = [activation]
+      
+      zs = []
+
+      for w, b in zip(self.weights, self.biases):
+         z = np.dot(w, activation) + b
+         zs.append(z)
+         activation = self.sigmoid(z)
+         activations.append(activation)
+
+      return activations, zs
+   
+   # output error
+   # backrop through hidden layers
+   def backpropagation(self, x, y):
+      nabla_w = [np.zeros(w.shape) for w in self.weights]
+      nabla_b = [np.zeros(b.shape) for b in self.biases]
+
+      activations, zs = self.forward_pass(x)
+
+      delta = (activations[-1] - y) * self.sigmoid_prime(zs[-1])
+      nabla_b[-1] = delta
+      nabla_w[-1] = np.outer(delta, activations[-2])
+
+      for l in range(2, self.num_layers):
+         z = zs[-l]
+         sp = self.sigmoid_prime(z)
+         delta = np.dot(self.weights[-l+1].T, delta) * sp
+         nabla_b[-l] = delta
+         nabla_w[-l] = np.outer(delta, activations[-l-1])
+
+      return nabla_w, nabla_b
 
 loader = DatasetLoader(galaxy_dir='./images/galaxies', non_galaxy_dir='./images/non_galaxies', preprocessor=ImagePreprocessor(), flatten=True)
 data, labels = loader.load()
@@ -29,3 +63,6 @@ output = net.forward_pass(data[0])
 print('Output:', output)
 loss = net.binary_cross_entropy(labels[0], output)
 print('Loss:', loss)
+nabla_w, nabla_b = net.backpropagation(data, labels)
+print('Delta weights:', nabla_w)
+print('Delta biases:', nabla_b)
